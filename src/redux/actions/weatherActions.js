1f3314b7a44de9d05cd57dd8f020de3ee4077a55
Al-Amin Jibril <69981351+alaminjibril@users.noreply.github.com>
2025-03-28 02:43:44 +0100

    Update weatherActions.js

--- src/redux/actions/weatherActions.js
+++ src/redux/actions/weatherActions.js
@@ -1,16 +1,21 @@
+// Import the getWeather function from the weather service utility
 import { getWeather } from "../../utils/weatherService";
 
+// Define action types for weather fetching
 export const FETCH_WEATHER_REQUEST = "FETCH_WEATHER_REQUEST";
-export const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS";
-export const FETCH_WEATHER_ERROR = "FETCH_WEATHER_ERROR";
+export const FETCH_WEATHER_SUCCESS = "FETCH_WEATHER_SUCCESS"; 
+export const FETCH_WEATHER_ERROR = "FETCH_WEATHER_ERROR"; 
 
+// Thunk action to fetch weather data for a given city
 export const fetchWeather = (city) => async (dispatch) => {
-  dispatch({ type: FETCH_WEATHER_REQUEST });
+  // Notify Redux store that weather data fetching has started
+  dispatch({ type: FETCH_WEATHER_REQUEST }); 
 
   try {
-    const weatherData = await getWeather(city);
-    dispatch({ type: FETCH_WEATHER_SUCCESS, payload: weatherData });
+    // Call the weather API service to fetch data
+    const weatherData = await getWeather(city); 
+    dispatch({ type: FETCH_WEATHER_SUCCESS, payload: weatherData }); 
   } catch (error) {
-    dispatch({ type: FETCH_WEATHER_ERROR, payload: error.message });
+    dispatch({ type: FETCH_WEATHER_ERROR, payload: error.message }); 
   }
 };
