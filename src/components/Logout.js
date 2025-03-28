b5146b7cea23f580151e1862e9c50c27921b4b8b
Al-Amin Jibril <69981351+alaminjibril@users.noreply.github.com>
2025-03-28 11:12:09 +0100

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,4 +1,3 @@
-// src/components/Logout.js
 import React from "react";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../redux/actions/authActions";
@@ -8,12 +7,14 @@ const Logout = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // Handles user logout
   const handleLogout = () => {
-    dispatch(logoutUser());
-    navigate("/login"); // Redirect to login page
+    dispatch(logoutUser()); // Dispatches logout action to Redux store
+    navigate("/login"); // Redirects user to the login page after logout
   };
 
   return (
+    // Logout button that triggers the handleLogout function
     <button onClick={handleLogout} className="btn btn-danger">
       Logout
     </button>
